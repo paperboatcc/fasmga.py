392524e54057991cc1606dea9bea0f6222428497
StuckDuck <stuck@stuckduck.it>
2022-02-20 13:16:51 +0100

    Refactored code

--- fasmga/url.py
+++ fasmga/url.py
@@ -1,28 +1,41 @@
-
-
-class URL:
-    def __init__(self, client, **kwargs):
+class Url:
+    def __init__(self,
+        client,
+        gash: str, redirect: str,
+        nsfw: bool = False,
+        clicks: int = 0,
+    *args, **kwargs):
         from .client import Client
 
         self.client: Client = client
-        self.id = kwargs["ID"].strip("/")
-        self.uri = kwargs["redirect_url"]
-        self.nsfw = kwargs["nsfw"]
-        self.clicks = kwargs.get("clicks", 0)
+        self.gash = gash.strip("/")
+        self.uri = redirect
+        self.is_nsfw = nsfw
+        self.clicks = clicks
 
-    def __str__(self):
-        return f"https://fasm.ga/{self.id}"
+    def __str__(self,
+        mode: str = "short"
+    ):
+        root_url = "fga.sh" if mode is short else "fasmga.org"
+        return f"https://{root_url}/{self.gash}"
 
     def __repr__(self):
-        return f"<URL id='{self.id}' uri='{self.uri}' nsfw={self.nsfw}>"
+        return f"<url gash='{self.gash}' redirect='{self.redirect}' nsfw={self.is_nsfw}>"
 
-    async def edit(self, *, url=None, password=None, nsfw=False):
-        await self.client.edit(self.id, url=url, password=password, nsfw=nsfw)
-        self.uri = url
-        self.nsfw = nsfw
+    async def edit(self,
+        redirect: str = None,
+        password: str = None,
+        nsfw: bool = False
+    *args, **kwargs):
+        await self.client.edit(
+            self.gash,
+            url = redirect,
+            password = password, nsfw = nsfw)
+        self.uri = redirect
+        self.is_nsfw = nsfw
     
     async def delete(self):
         self.client._pending_deletion_urls.append(self)
-        await self.client.delete(self.id)
+        await self.client.delete(self.gash)
         self.client.urls.remove(self)
         self.client._pending_deletion_urls.remove(self)
